ba1c705c2feab553c88ce5ae5c0d0bd721ced8ba
Douglas da Costa <douglas.costa84@gmail.com>
2016-07-25 01:13:30 -0300

    Implementation of a stack

---
+++ stack.py
@@ -0,0 +1,39 @@
+class Stack:
+    class Node:
+        def __init__(self, value):
+            self.value = value
+            self.next  = None
+
+    def __init__(self):
+        self.head = None
+
+    def empty(self):
+        return self.head is None
+
+    def push(self, value):
+        node      = self.Node(value)
+        node.next = self.head
+        self.head = node
+
+    def pop(self):
+        if self.empty():
+            return None
+
+        node      = self.head
+        self.head = self.head.next
+
+        return node
+
+    def elements(self):
+        node = self.head
+
+        while node:
+            yield node.value
+            node = node.next
+
+    def show(self):
+        if self.empty():
+            print('The stack is empty')
+            return
+
+        print(' - '.join(str(i) for i in self.elements()))
